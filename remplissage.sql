15943e4de66fccf259a48781ec8180e748ed3435
OliveValentin <olive.valentin@gmail.com>
2014-05-30 16:17:58 +0200

    Changement base de données
    Changement dans la date du vin attribut mis dans la table
    bouteille_has_vin et supprimer de la table vin.
    Creation d'un fichier requete pour les requete superflues à la fin du
    fichier remplissage

--- remplissage.sql
+++ remplissage.sql
@@ -1,9 +1,9 @@
 USE cave;
 
-INSERT INTO Vins VALUES(1,'alsace','zind','chateauvin1','rouge','syrah',1956);
-INSERT INTO Vins VALUES(2,'beaujolais','domainevin2','montmelas','blanc','grenache',1963);
-INSERT INTO Vins VALUES(3,'jura','domainevin3','arlay','rose','muscat',1974);
-INSERT INTO Vins VALUES(4,'bordeaux','chevalier','chateauvin4','rose','riesling',1822);
+INSERT INTO Vins VALUES(1,'alsace','zind','chateauvin1','rouge','syrah');
+INSERT INTO Vins VALUES(2,'beaujolais','domainevin2','montmelas','blanc','grenache');
+INSERT INTO Vins VALUES(3,'jura','domainevin3','arlay','rose','muscat');
+INSERT INTO Vins VALUES(4,'bordeaux','chevalier','chateauvin4','rose','riesling');
 
 
 INSERT INTO Utilisateur VALUES(1, 'nomuser1', MD5('mdpuser1'));
@@ -20,27 +20,7 @@ INSERT INTO Bouteille VALUES(2, 225);
 INSERT INTO Bouteille VALUES(3, 250);
 INSERT INTO Bouteille VALUES(4, 150);
 
-INSERT INTO Bouteille_has_Vins VALUES(1, 1, 1);
-INSERT INTO Bouteille_has_Vins VALUES(2, 4, 2);
-INSERT INTO Bouteille_has_Vins VALUES(3, 2, 2);
-INSERT INTO Bouteille_has_Vins VALUES(4, 3, 3);
-
-
-select * from vins;
-select * from utilisateur;
-select * from caveAVin;
-select * from etagère;
-select * from bouteille;
-select * from bouteille_has_vins;
-
-UPDATE vins SET identifiantVin='7' WHERE identifiantVin=2;
-DELETE FROM bouteille WHERE identifiantBouteille=1;
-
-DROP TABLE Bouteille_has_Vins;
-DROP TABLE vins ;
-DROP TABLE Bouteille;
-DROP TABLE Etagère;
-DROP TABLE CaveAVin;
-DROP TABLE Utilisateur;
-
-DROP SCHEMA Cave;
+INSERT INTO Bouteille_has_Vins VALUES(1, 1, 1956, 1);
+INSERT INTO Bouteille_has_Vins VALUES(2, 4, 1822, 2);
+INSERT INTO Bouteille_has_Vins VALUES(3, 2, 1963, 2);
+INSERT INTO Bouteille_has_Vins VALUES(4, 3, 1974, 3);
